9b04e28f425fc22c9d0b65534ec37f5e8bbe73b0
KramYengir <kramyengir@gmail.com>
2024-06-24 16:11:12 +0100

    add shadcnui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Merriweather } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+// const merriweather = Merriweather({
+//   weight: ["300", "400", "700"],
+//   subsets: ["latin"],
+//   variable: "--font-merriweather",
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <header className="min-h-screen bg-fh-blue-800"></header>;
+};
+
+export default Header;
