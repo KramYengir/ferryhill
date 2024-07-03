7566105b1f0d725b665302947661c5ae676694ad
KramYengir <kramyengir@gmail.com>
2024-07-03 14:36:48 +0100

    remove everything but hero for now

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,11 @@
+import Hero from "@/components/Hero";
 import heroImg from "../assets/images/hero-img.png";
 import Image from "next/image";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main className="flex flex-col min-h-screen">
-      <section className="min-h-[80vh] flex justify-center items-start  bg-fh-blue-950">
-        <div className="relative w-[80vw] aspect-square sm:max-w-sm my-32 backdrop-blur-md">
-          <Image
-            src={heroImg}
-            fill
-            sizes="100vh"
-            alt="welcome message for ferryhill"
-          />
-        </div>
-      </section>
-
-      {/* bg for both navbar and header */}
-      {/* <div className="absolute -z-10 top-0 right-0 left-0 -bottom-28 bg-hero  bg-cover bg-no-repeat bg-fixed"></div> */}
+      <Hero />
     </main>
   );
 }
