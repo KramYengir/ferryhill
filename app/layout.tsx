04ffd4cd64bdac606001ce54a76b4d5e38d877d0
KramYengir <kramyengir@gmail.com>
2024-07-03 14:34:57 +0100

    add light bg to body

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>
+      <body className={`${inter.className} bg-fh-gold-50`}>
         <Nav />
         {children}
       </body>
