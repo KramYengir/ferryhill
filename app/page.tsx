8c1f3b7caf5dcc88931a3a6ee7109096c41ac696
KramYengir <kramyengir@gmail.com>
2024-07-09 14:40:40 +0100

    refactored review section into component

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import ScrollAnimatedSection from "@/components/ScrollAnimatedSection";
 import paintingIMG from "@/assets/images/painting.jpg";
 import fishIMG from "@/assets/images/fish_chips.png";
 import fishMealIMG from "@/assets/images/FH_fish-meal.jpeg";
+import ReviewsSection from "@/components/ReviewsSection";
 
 export default function HomePage() {
   return (
@@ -14,7 +15,7 @@ export default function HomePage() {
       <div className="bg-[url('../assets/images/tiled_bg.png')] bg-cover bg-center bg-no-repeat">
         <section className="flex flex-wrap md:gap-16 justify-center max-w-screen-xl text-left py-20 px-12 mx-auto bg-white bg-opacity-10">
           <div className="flex-1 flex justify-center items-center min-w-72 mb-8">
-            <Image src={fishIMG} alt="fish and chips ina  tray"></Image>
+            <Image src={fishIMG} alt="fish and chips in a tray"></Image>
           </div>
           <article className="max-w-sm flex flex-col  justify-center">
             <h2 className="text-balance text-fh-blue-700 text-4xl uppercase font-bold tracking-wide">
@@ -35,27 +36,28 @@ export default function HomePage() {
       </div>
 
       {/* second section */}
-      <div className="bg-[url('../assets/images/FH_fish-meal.jpeg')] bg-cover bg-center bg-fixed bg-no-repeat">
-        <div className="bg-fh-blue-700 bg-opacity-85 ">
-          <section className="flex flex-wrap flex-row-reverse md:gap-16 justify-center max-w-screen-xl text-left py-20 px-12 mx-auto ">
-            <div className="flex-1 flex justify-center items-center min-w-72 mb-8">
-              <Image src={fishIMG} alt="fish and chips ina  tray"></Image>
-            </div>
-            <article className="max-w-sm flex flex-col  justify-center">
-              <h2 className="text-balance text-white text-4xl uppercase font-bold tracking-wide">
-                Fresh, Sustainable, Line-Caught Fish Delivered Daily
-              </h2>
-              <hr className=" border-b-1 border-white my-2" />
-              <p className="text-balance text-white">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry standard dummy text
-                ever since the 1500s, when an unknown printer took a galley of
-                type and scrambled it to make a type specimen book.{" "}
-              </p>
-              <button className=" btn-secondary mt-4">Our Menu</button>
-            </article>
-          </section>
-        </div>
+      <ReviewsSection />
+
+      {/* third section */}
+      <div className="bg-[url('../assets/images/tiled_bg.png')] bg-cover bg-center bg-fixed bg-no-repeat">
+        <section className="flex flex-wrap flex-row-reverse md:gap-16 justify-center max-w-screen-xl text-left py-20 px-12 mx-auto ">
+          <div className="flex-1 flex justify-center items-center min-w-72 mb-8">
+            <Image src={fishIMG} alt="fish and chips ina  tray"></Image>
+          </div>
+          <article className="max-w-sm flex flex-col  justify-center">
+            <h2 className="text-balance text-fh-blue-700 text-4xl uppercase font-bold tracking-wide">
+              Fresh, Sustainable, Line-Caught Fish Delivered Daily
+            </h2>
+            <hr className=" border-b-1 border-white my-2" />
+            <p className="text-balance ">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.{" "}
+            </p>
+            <button className=" btn-primary mt-4">Our Menu</button>
+          </article>
+        </section>
       </div>
     </main>
   );
