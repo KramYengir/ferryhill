b829d31fd933994b77ae2884593c50f779c9f8d5
KramYengir <kramyengir@gmail.com>
2024-07-11 13:54:43 +0100

    tidy hours section & add emphasis to reputation h2

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,11 @@ import ScrollAnimatedSection from "@/components/utility/ScrollAnimatedSection";
 import paintingIMG from "@/assets/images/painting.jpg";
 import fishIMG from "@/assets/images/fish_chips.png";
 import fishMealIMG from "@/assets/images/FH_fish-meal.jpeg";
+import potatoesIMG from "@/assets/images/FH_potatoes.png";
+import hoursIMG from "@/assets/images/FH_hours.png";
 import ReviewsSection from "@/components/ReviewsSection";
 import AccreditationSection from "@/components/AccreditationSection";
+import OpeningHours from "@/components/OpeningHours";
 
 export default function HomePage() {
   return (
@@ -14,14 +17,17 @@ export default function HomePage() {
       <Hero />
 
       <div className="bg-[url('../assets/images/tiled_bg.png')] bg-cover bg-center bg-no-repeat">
-        <section className="flex flex-wrap md:gap-16 justify-center max-w-screen-xl text-left py-20 px-12 mx-auto bg-white bg-opacity-10">
+        <section
+          id="reputation"
+          className="flex flex-wrap md:gap-16 justify-center max-w-screen-xl text-left py-20 px-12 mx-auto bg-white bg-opacity-10"
+        >
           <div className="flex-1 flex justify-center items-center min-w-72 mb-8">
             <Image src={fishIMG} alt="fish and chips in a tray"></Image>
           </div>
-          <article className="max-w-sm flex flex-col  justify-center">
+          <article className="max-w-sm flex flex-col justify-center">
             <h2 className="text-balance text-fh-blue-700 text-4xl uppercase font-bold tracking-wide">
               Voted Salfords{" "}
-              <span className="text-fh-gold-500 italic">Best Chippy</span> in
+              <span className="text-fh-gold-500 text-5xl">Best Chippy</span> in
               2023!
             </h2>
             <hr className=" border-b-1 border-fh-blue-900 my-2" />
@@ -41,22 +47,18 @@ export default function HomePage() {
 
       {/* third section */}
       <div className="bg-[url('../assets/images/tiled_bg.png')] bg-cover bg-center bg-fixed bg-no-repeat">
-        <section className="flex flex-wrap  md:gap-16 justify-center max-w-screen-xl text-left py-20 px-12 mx-auto ">
-          <div className="flex-1 flex justify-center items-center min-w-72 mb-8">
-            <Image src={fishIMG} alt="fish and chips ina  tray"></Image>
-          </div>
-          <article className="max-w-sm flex flex-col  justify-center">
-            <h2 className="text-balance text-fh-blue-700 text-4xl uppercase font-bold tracking-wide">
-              Fresh, Sustainable, Line-Caught Fish Delivered Daily
+        <section className="flex flex-row-reverse flex-wrap gap-8 items-center justify-evenly max-w-screen-xl text-left py-20 px-12 mx-auto ">
+          <OpeningHours />
+          <article className="max-w-sm flex flex-col gap-4 justify-center text-center bg-white bg-opacity-10  rounded-md p-8 border-2 border-fh-blue-700  ">
+            <h2 className="text-balance text-fh-blue-700 text-4xl uppercase font-bold tracking-wide text-center">
+              Opening hours
             </h2>
-            <hr className=" border-b-1 border-white my-2" />
-            <p className="text-balance ">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.{" "}
+            {/* <hr className=" border-b-1 border-fh-blue-900 my-2" /> */}
+            <p className="text-balance text-lg  max-w-prose">
+              Be sure to check our social media pages or get in touch with us
+              for any up-to-date changes{" "}
             </p>
-            <button className=" btn-primary mt-4">Our Menu</button>
+            <button className=" btn-primary mt-4">Contact us</button>
           </article>
         </section>
       </div>
