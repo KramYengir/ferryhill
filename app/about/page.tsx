244e506cfa15f16a11344ad30c6fe7f86fed5447
KramYengir <kramyengir@gmail.com>
2024-08-05 17:56:50 +0100

    add about route

---
+++ app/about/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div className="w-full max-w-screen-xl text-left px-6  sm:px-20 mx-auto">
+      <h1 className="text-8xl uppercase font-extrabold tracking-wide text-center bg-clip-text text-transparent my-16  bg-[url('../assets/images/fish-in-oil.png')] bg-cover bg-center bg-no-repeat bg-fixed">
+        Our Story
+      </h1>
+      <div className="">
+        <h2 className=" capitalize text-2xl text-fh-blue-800 font-bold mb-4 text-center">
+          our beginning...
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
