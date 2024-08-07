e86b5474078f3b623888f03eaeb1d8b529c7a5bd
KramYengir <kramyengir@gmail.com>
2024-08-07 19:33:47 +0100

    try masonry grid for menu

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -8,9 +8,11 @@ const MenuPage = () => {
       <h1 className="text-8xl uppercase font-extrabold tracking-wide text-center bg-clip-text text-transparent my-16  bg-[url('../assets/images/fish-in-oil.png')] bg-cover bg-center bg-no-repeat bg-fixed">
         Our Menu
       </h1>
-      {menuItems.map((category) => (
-        <MenuCategory key={category.name} category={category} />
-      ))}
+      <div className="columns-1 md:columns-2 xl:columns-3 mt-8">
+        {menuItems.map((category) => (
+          <MenuCategory key={category.name} category={category} />
+        ))}
+      </div>
     </div>
   );
 };
--- components/MenuCategory.tsx
+++ components/MenuCategory.tsx
@@ -22,14 +22,14 @@ const MenuCategory: React.FC<MenuCategoryProps> = ({ category }) => {
         {category.name}
       </h2>
       {category.info && (
-        <p className="text-xl px-2 max-w-prose font-light mt-2">
+        <p className="text-sm px-2 max-w-prose font-light mt-2">
           {category.info}
         </p>
       )}
 
-      <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 mt-4">
+      <div className="mt-4">
         {category.items.map((item) => (
-          <div key={item.id} className="bg-white p-4 rounded shadow">
+          <div key={item.id} className="bg-white p-4 mb-4 rounded shadow">
             <div className="flex justify-between items-center">
               <h3 className="text-lg font-semibold">{item.name}</h3>
               {item.price !== undefined && (
@@ -43,9 +43,7 @@ const MenuCategory: React.FC<MenuCategoryProps> = ({ category }) => {
                 {item.sizes.map((size, index) => (
                   <li key={index} className="flex justify-between">
                     <span>{size.size}</span>
-                    <span className="text-lg font-semibold">{`£${size.price.toFixed(
-                      2
-                    )}`}</span>
+                    <span>{`£${size.price.toFixed(2)}`}</span>
                   </li>
                 ))}
               </ul>
