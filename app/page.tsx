a62c0a8abbd04749cf03e620912150bf51380124
KramYengir <kramyengir@gmail.com>
2024-08-07 19:12:44 +0100

    replace 1st section with nutshell cards

--- app/page.tsx
+++ app/page.tsx
@@ -4,37 +4,13 @@ import fishIMG from "@/assets/images/fish_chips.png";
 import ReviewsSection from "@/components/ReviewsSection";
 import AccreditationSection from "@/components/AccreditationSection";
 import OpeningHours from "@/components/OpeningHours";
+import NutshellCards from "@/components/NutshellCards";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
-
-      <div className="">
-        <section
-          id="reputation"
-          className="fade-in-element flex flex-wrap md:gap-16 justify-center max-w-screen-xl text-left py-20 px-12 mx-auto bg-white bg-opacity-10"
-        >
-          <div className="flex-1 flex justify-center items-center min-w-72 mb-8">
-            <Image src={fishIMG} alt="fish and chips in a tray"></Image>
-          </div>
-          <article className="max-w-sm flex flex-col gap-4 justify-center">
-            <h2 className="text-balance text-fh-blue-700 text-4xl uppercase font-bold tracking-wide">
-              Voted Salfords{" "}
-              <span className="text-fh-gold-500 text-5xl">Best Chippy</span> in
-              2023!
-            </h2>
-            {/* <hr className=" border-b-1 border-fh-blue-900 my-2" /> */}
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.{" "}
-            </p>
-            <button className=" btn-primary mt-4">About Us</button>
-          </article>
-        </section>
-      </div>
+      <NutshellCards />
 
       {/* second section */}
       <ReviewsSection />
