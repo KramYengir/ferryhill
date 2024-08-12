5336db5914df19a76b3b64dabe1f371a8d73eacb
KramYengir <kramyengir@gmail.com>
2024-08-12 14:37:44 +0100

    refactor cards into seperate component

---
+++ components/NutshellCard.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Link from "next/link";
+import { LucideIcon } from "lucide-react";
+
+// Define the interface for the Card props
+interface CardProps {
+  icon: LucideIcon;
+  text: string;
+  link: string;
+  linkText: string;
+}
+
+const Card: React.FC<CardProps> = ({ icon: Icon, text, link, linkText }) => {
+  return (
+    <div className="flex flex-col items-center gap-8 px-6 py-8 rounded-md shadow-lg shadow-fh-blue-950/25">
+      <Icon size={48} className="text-fh-blue-700" />
+      <p className="text-center max-w-60">{text}</p>
+      <Link href={link} className="btn-primary">
+        {linkText}
+      </Link>
+    </div>
+  );
+};
+
+export default Card;
--- components/NutshellCards.tsx
+++ components/NutshellCards.tsx
@@ -1,6 +1,7 @@
 import { Award, Earth, Fish, Vegan } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import NutshellCard from "./NutshellCard";
 
 const NutshellCards = () => {
   return (
@@ -10,49 +11,37 @@ const NutshellCards = () => {
       </h2>
       <p className="text-center text-xl font-light">In a nutshell</p>
       <div className="flex flex-wrap justify-evenly gap-y-16 max-w-screen-2xl mx-auto py-32">
-        <div className="flex flex-col items-center gap-8 px-6 py-8 rounded-md shadow-lg shadow-fh-blue-950/25">
-          <Award size={48} className="text-fh-blue-700" />
-          <p className="text-center max-w-60">
-            Voted Best Fish & Chips in Salford in 2023, with consistent 5 star
+        <NutshellCard
+          icon={Award}
+          text="Voted Best Fish & Chips in Salford in 2023, with consistent 5 star
             reviews on Google, we have a reputation for serving some of the best
-            food in Manchester
-          </p>
-          <Link href={"#reviews"} className="btn-primary">
-            Our Reviews
-          </Link>
-        </div>
-        <div className="flex flex-col items-center gap-8 px-6 py-8 rounded-md shadow-lg shadow-fh-blue-950/25">
-          <Fish size={48} className="text-fh-blue-700" />
-          <p className="text-center max-w-60">
-            Our fish is sourced from sustainable fishing grounds, delivered
+            food in Manchester"
+          link="#reviews"
+          linkText="Our Reviews"
+        />
+        <NutshellCard
+          icon={Fish}
+          text="Our fish is sourced from sustainable fishing grounds, delivered
             fresh every day, and cooked to perfection in 100% Uncontaminated
-            Vegetable oil, filtered every day
-          </p>
-          <Link href={"/menu"} className="btn-primary">
-            Our Menu
-          </Link>
-        </div>
-        <div className="flex flex-col items-center gap-8 px-6 py-8 rounded-md shadow-lg shadow-fh-blue-950/25">
-          <Earth size={48} className="text-fh-blue-700" />
-          <p className="text-center max-w-60">
-            All of our packaging & Utensils are 100% recyclable & Biodegradable,
-            and we encourage our customers to reuse our bags whenever possible
-          </p>
-          <Link href={"/about"} className="btn-primary">
-            About Us
-          </Link>
-        </div>
-        <div className="flex flex-col items-center gap-8 px-6 py-8 rounded-md shadow-lg shadow-fh-blue-950/25">
-          <Vegan size={48} className="text-fh-blue-700" />
-          <p className="text-center max-w-60">
-            With Gluten-Free options every Monday & Tuesday, and Vegan &
+            Vegetable oil, filtered every day"
+          link="/menu"
+          linkText="Our Menu"
+        />
+        <NutshellCard
+          icon={Earth}
+          text="All of our packaging & Utensils are 100% recyclable & Biodegradable,
+            and we encourage our customers to reuse our bags whenever possible"
+          link="/about"
+          linkText="About Us"
+        />
+        <NutshellCard
+          icon={Vegan}
+          text="With Gluten-Free options every Monday & Tuesday, and Vegan &
             Vegetarian options every day, we are proud to offer a wide range of
-            choices to suit all dietary requirements
-          </p>
-          <Link href={"/contact"} className="btn-primary">
-            Contact Us
-          </Link>
-        </div>
+            choices to suit all dietary requirements"
+          link="/contact"
+          linkText="Contact Us"
+        />
       </div>
     </section>
   );
