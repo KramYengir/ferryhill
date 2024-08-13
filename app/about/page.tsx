6f3a1bdd7118f72d02a4eea4630a45df1cbe0938
KramYengir <kramyengir@gmail.com>
2024-08-13 16:11:55 +0100

    change bg to neutral

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ import { aboutInfo } from "../../data/aboutInfo";
 const AboutPage = () => {
   return (
     <div className="grid w-full bg-[url('../assets/images/FH_fish_meal.jpeg')] bg-cover bg-center bg-no-repeat bg-fixed">
-      <div className="bg-white">
+      <div className="bg-neutral-50">
         <h1 className="text-8xl uppercase font-extrabold tracking-wide text-center bg-clip-text text-transparent my-24 bg-[url('../assets/images/fish-in-oil.webp')] bg-cover bg-center bg-no-repeat bg-fixed">
           Our Story
         </h1>
