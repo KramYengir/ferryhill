990c1cc39169081b933d841fa5cdda85871b7bfe
KramYengir <kramyengir@gmail.com>
2024-08-13 16:12:09 +0100

    begin contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ContactPage = () => {
+  return (
+    <div className="grid w-full bg-[url('../assets/images/FH_fish_meal.jpeg')] bg-cover bg-center bg-no-repeat bg-fixed">
+      <div className="bg-neutral-50">
+        <h1 className="text-8xl uppercase font-extrabold tracking-wide text-center bg-clip-text text-transparent my-24 bg-[url('../assets/images/fish-in-oil.webp')] bg-cover bg-center bg-no-repeat bg-fixed">
+          Contact Us
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default ContactPage;
