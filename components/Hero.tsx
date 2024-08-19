5fb55b2ba98b1119917e5b9ba0c9fd2a6a6b9db1
KramYengir <kramyengir@gmail.com>
2024-08-19 12:10:28 +0100

    crop image for easier styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,15 +10,15 @@ const Hero = () => {
       id="hero"
       className="flex flex-col  bg-[url('../assets/images/fish_heater_portrait_2.webp')] md:bg-[url('../assets/images/fish_meal.webp')] bg-cover bg-center bg-no-repeat bg-fixed"
     >
-      <div className="flex flex-col items-center text-center pt-12 pb-24 ">
-        <div className=" w-[80vw] aspect-square sm:max-w-sm">
+      <div className="flex flex-col  items-center text-center py-24 ">
+        <div className=" w-[80vw]  sm:max-w-sm">
           <Image src={heroImg} alt="welcome message for ferryhill" />
           <h1 className="sr-only">Welcome to our Ferryhill Fish and Chips</h1>
           <p className="sr-only">
             Traditional fish and chips in the heart of irlam, manchester
           </p>
         </div>
-        <div className="flex gap-6 mb-24">
+        <div className="flex gap-6 mt-12 mb-24">
           <Link href={"/menu"} className=" btn-primary">
             Our Menu
           </Link>
