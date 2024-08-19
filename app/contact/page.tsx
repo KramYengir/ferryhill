587969ea28fde40a9c0160406544cca9808cfafb
KramYengir <kramyengir@gmail.com>
2024-08-19 12:21:02 +0100

    refactor cards into own component

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,6 +2,7 @@ import { FacebookIcon, Instagram, Mail, MapPin, PhoneIcon } from "lucide-react";
 import React from "react";
 import GoogleMapsComponent from "@/components/GoogleMaps";
 import Link from "next/link";
+import ContactCard from "@/components/ContactCard";
 
 const ContactPage = () => {
   return (
@@ -15,79 +16,53 @@ const ContactPage = () => {
 
       {/* How to get in touch */}
       <section className="flex flex-wrap lg:flex-nowrap justify-evenly items-start gap-x-6 gap-y-0 sm:pb-24 text-center bg-neutral-50 text-fh-blue-900">
-        <div className="grid justify-center items-center min-h-96 px-8 py-12 shadow-lg rounded-md">
-          <h2 className="text-xl font-extrabold uppercase tracking-widest text-fh-blue-600">
-            telephone
-          </h2>
-          <p className="max-w-sm leading-relaxed">
-            If you have any enquiries, you can give us a call during opening
-            hours and we&apos;ll be glad to help!
-          </p>
-          <div>
+        <ContactCard
+          title="telephone"
+          description="If you have any enquiries, you can give us a call during opening hours and we'll be glad to help!"
+          icon={
             <PhoneIcon size={32} className="mx-auto mb-2 text-fh-gold-500" />
-            <a
-              href="tel:01617754738"
-              className="text-lg text-fh-blue-900 tracking-wide font-light"
-            >
-              0161 775 4738
-            </a>
-          </div>
-        </div>
-        <div className="grid justify-center items-center min-h-96 px-8 py-12 shadow-lg rounded-md">
-          <h2 className="text-xl font-extrabold uppercase tracking-widest text-fh-blue-600">
-            email
-          </h2>
-          <p className="max-w-sm leading-relaxed">
-            If you&apos;re not in a rush, then send us an email and we&apos;ll
-            get back to you as soon as possible!
-          </p>
-          <div>
-            <Mail size={32} className="mx-auto mb-2 text-fh-gold-500" />
-            <a
-              href="email:ferryhillfishandchips@gmail.com"
-              className="text-lg text-fh-blue-900 tracking-wide font-light"
-            >
-              ferryhillfishandchips@gmail.com
-            </a>
-          </div>
-        </div>
-        <div className="grid justify-center items-center min-h-96 px-8 py-12 shadow-lg rounded-md">
-          <h2 className="text-xl font-extrabold uppercase tracking-widest text-fh-blue-600">
-            social media
-          </h2>
-          <p className="max-w-sm leading-relaxed">
-            To keep up-to-date with changes to our opening hours or any
-            announcements, please check our Social Media!
-          </p>
-          <div className="flex justify-evenly">
-            <Link href={"https://www.instagram.com/ferryhill_fishchips/"}>
-              <Instagram size={48} className="mx-auto text-fh-gold-500" />
-            </Link>
-            <Link
-              href={
-                "https://www.facebook.com/FerryhillFishChips/?ref=embed_page"
-              }
-            >
-              <FacebookIcon
-                size={48}
-                className="mb-4 mx-auto text-fh-gold-500"
-              />
-            </Link>
-          </div>
-        </div>
+          }
+          link="tel:01617754738"
+          linkText="0161 775 4738"
+        />
+        <ContactCard
+          title="email"
+          description="If you're not in a rush, then send us an email and we'll get back to you as soon as possible!"
+          icon={<Mail size={32} className="mx-auto mb-2 text-fh-gold-500" />}
+          link="mailto:ferryhillfishandchips@gmail.com"
+          linkText="ferryhillfishandchips@gmail.com"
+        />
+        <ContactCard
+          title="social media"
+          description="To keep up-to-date with changes to our opening hours or any announcements, please check our Social Media!"
+          icon={
+            <>
+              <Link href="https://www.instagram.com/ferryhill_fishchips/">
+                <Instagram size={48} className="mx-auto text-fh-gold-500" />
+              </Link>
+              <Link href="https://www.facebook.com/FerryhillFishChips/?ref=embed_page">
+                <FacebookIcon
+                  size={48}
+                  className="mb-4 mx-auto text-fh-gold-500"
+                />
+              </Link>
+            </>
+          }
+          isSocialMedia={true}
+        />
       </section>
 
       {/* Address */}
-      <section className="bg-gradient-to-b sm:bg-gradient-to-r from-neutral-900 to-neutral-900/80 text-white py-24">
+      <section className="bg-gradient-to-b sm:bg-gradient-to-r from-neutral-900 to-neutral-900/90 text-white py-24">
         <h2 className="uppercase tracking-widest font-semibold text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl text-center pb-6 sm:pb-24">
           Where to find us
         </h2>
         <div className="flex flex-wrap justify-evenly items-start gap-x-6 gap-y-12">
-          <div className="grid justify-center items-center gap-6  pt-6 sm:pt-12">
+          <div className="grid justify-center items-center gap-6 pt-6 sm:pt-12">
             <h3 className="sr-only">Address</h3>
             <MapPin size={32} className="mx-auto sm:hidden text-fh-blue-500" />
             <p className="text-lg md:text-xl tracking-wide font-light text-center sm:text-left">
-              <span className="font-semibold">Ferryhill Fish & Chips</span>{" "}
+              <span className="font-semibold">Ferryhill Fish & Chips</span>
               <br /> 50 Ferryhill Road,
               <br /> Irlam, <br />
               Manchester,
@@ -95,7 +70,7 @@ const ContactPage = () => {
               <br /> M44 6DD
             </p>
           </div>
-          <div className="w-full sm:w-2/4 min-w-96 h-96  ">
+          <div className="w-full sm:w-2/4 min-w-96 h-96">
             <GoogleMapsComponent />
           </div>
         </div>
---
+++ components/ContactCard.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Link from "next/link";
+
+interface ContactCardProps {
+  title: string;
+  description: string;
+  icon: React.ReactNode;
+  link?: string;
+  linkText?: string;
+  isSocialMedia?: boolean;
+}
+
+const ContactCard: React.FC<ContactCardProps> = ({
+  title,
+  description,
+  icon,
+  link,
+  linkText,
+  isSocialMedia = false,
+}) => {
+  return (
+    <div className="grid justify-center items-center min-h-96 px-8 py-12 sm:shadow-lg rounded-md">
+      <h2 className="text-xl font-extrabold uppercase tracking-widest text-fh-blue-600">
+        {title}
+      </h2>
+      <p className="max-w-sm leading-relaxed">{description}</p>
+      <div
+        className={`flex ${
+          isSocialMedia ? "justify-evenly" : "flex-col items-center"
+        }`}
+      >
+        {link ? (
+          <a
+            href={link}
+            className="text-lg text-fh-blue-900 tracking-wide font-light"
+          >
+            {icon}
+            {linkText && !isSocialMedia && <span>{linkText}</span>}
+          </a>
+        ) : (
+          icon
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ContactCard;
