4efac2d5f3abe0a6ef37aad55156f92c93aca9a0
KramYengir <kramyengir@gmail.com>
2024-08-19 16:56:53 +0100

    change to flex-col

--- components/AccreditationSection.tsx
+++ components/AccreditationSection.tsx
@@ -17,7 +17,7 @@ const AccreditationSection = () => {
     <div className="bg-[url('../assets/images/FH_fish_heater.jpeg')] bg-cover bg-center bg-fixed bg-no-repeat">
       <div className="bg-neutral-950 bg-opacity-70 backdrop-blur-sm">
         <section className="flex flex-wrap gap-16  justify-evenly max-w-screen-xl text-center py-28 px-12 mx-auto ">
-          <Card className="flex-1 grid justify-items-center items-end bg-neutral-950 bg-opacity-65 min-w-[300px] max-w-sm">
+          <Card className="flex-1 flex flex-col items-center justify-evenly bg-neutral-950 bg-opacity-65 min-w-[300px] max-w-sm">
             <CardContent className="flex flex-col items-center max-w-xs">
               <Image
                 src={nfffIMG}
@@ -33,7 +33,7 @@ const AccreditationSection = () => {
               </h3>
             </CardFooter>
           </Card>
-          <Card className="flex-1 grid justify-items-center  items-end bg-neutral-950 bg-opacity-65 min-w-[300px] max-w-sm">
+          <Card className="flex-1 flex flex-col items-center justify-evenly bg-neutral-950 bg-opacity-65 min-w-[300px] max-w-sm">
             <CardContent className="flex flex-col items-center max-w-xs">
               <Image
                 src={foodHygieneIMG}
@@ -51,7 +51,7 @@ const AccreditationSection = () => {
               </h3>
             </CardFooter>
           </Card>
-          <Card className="flex-1 grid justify-items-center  items-end bg-neutral-950 bg-opacity-65 min-w-[300px] max-w-sm">
+          <Card className="flex-1 flex flex-col items-center justify-evenly bg-neutral-950 bg-opacity-65 min-w-[300px] max-w-sm">
             <CardContent className="flex flex-col items-center max-w-xs">
               <Image src={ecoIMG} alt="eco friendly tree "></Image>
             </CardContent>
