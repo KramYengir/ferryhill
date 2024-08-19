26e1cd3739631d266f0c5130d23e8a8cf3017d5e
KramYengir <kramyengir@gmail.com>
2024-08-19 13:04:45 +0100

    make nav links sticky when reach page top

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useEffect } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
@@ -14,17 +15,39 @@ const navLinks = [
 const Navbar = () => {
   const pathName = usePathname();
 
+  useEffect(() => {
+    // Make sure the element exists before accessing its properties
+    const navbar = document.querySelector("#NavBar") as HTMLElement | null;
+
+    if (!navbar) return; // If navbar is null, exit early
+
+    const top = navbar.offsetTop;
+    function stickynavbar() {
+      if (window.scrollY >= top) {
+        navbar?.classList.add("sticky");
+      } else {
+        navbar?.classList.remove("sticky");
+      }
+    }
+
+    window.addEventListener("scroll", stickynavbar);
+
+    return () => {
+      window.removeEventListener("scroll", stickynavbar);
+    };
+  }, []);
+
   return (
-    <header
-      id="header"
-      className="bg-[url('../assets/images/fish_heater_portrait_2.webp')] md:bg-[url('../assets/images/fish_meal.webp')] bg-cover bg-center bg-no-repeat bg-fixed "
-    >
-      <nav className="flex flex-col justify-center items-center pt-2 pb-4 px-4  mx-auto ">
+    <header className="bg-[url('../assets/images/fish_heater_portrait_2.webp')] md:bg-[url('../assets/images/fish_meal.webp')] bg-cover bg-center bg-no-repeat bg-fixed">
+      <nav className="flex flex-col justify-center items-center pt-2 pb-4 px-4 mx-auto">
         <Link href={"/"}>
           <Image src={logo} width={132} alt="logo of restaurant" />
         </Link>
 
-        <ul className="flex gap-6 px-6 py-2 bg-neutral-900 bg-opacity-60 rounded-xl overflow-hidden">
+        <ul
+          id="NavBar"
+          className="flex gap-6 px-6 py-2 bg-neutral-900 bg-opacity-60 rounded-xl overflow-hidden"
+        >
           {navLinks.map((link) => {
             const isActive = pathName === link.path;
             return (
