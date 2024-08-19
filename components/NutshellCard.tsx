46857eaa015c93f442b19522636fb7f0df89b82b
KramYengir <kramyengir@gmail.com>
2024-08-19 14:53:07 +0100

    make icon color lighter

--- components/NutshellCard.tsx
+++ components/NutshellCard.tsx
@@ -13,7 +13,7 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ icon: Icon, text, link, linkText }) => {
   return (
     <div className="flex flex-col items-center gap-8 px-6 py-8 rounded-md shadow-lg shadow-fh-blue-950/25">
-      <Icon size={48} className="text-fh-gold-600" />
+      <Icon size={48} className="text-fh-gold-500" />
       <p className="text-center max-w-60">{text}</p>
       <Link href={link} className="btn-primary">
         {linkText}
