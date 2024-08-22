9e382bec200bb02ef25d17588cfb6e8aa873caeb
KramYengir <kramyengir@gmail.com>
2024-08-22 13:28:53 +0100

    add new review

--- data/reviewsData.ts
+++ data/reviewsData.ts
@@ -8,15 +8,15 @@ export interface Review {
 export const reviews: Review[] = [
   {
     text: "Met by friendly staff who had lovely smiles on their faces as usual. The fish batter was crispy, the chips are still the best in Irlam, crispy batter from onion rings and a generous portion of scraps. Really enjoyed this. Best chippy around 👍",
-    author: "Tracy Townson",
+    author: "Tracy",
   },
   {
     text: "Heard about this place for a while now so gave it a go and wasn't disappointed , excellent staff and friendly, excellent service and fantastic food , definitely worth a revisit.",
-    author: "Andrew Brooks",
+    author: "Andrew",
   },
   {
     text: "Every time I come in this place I am served with well cooked and tasty fish. The place is always very clean and tidy and the personnel quite friendly with the customers. Recommended",
-    author: "Alexandros Oustamanolakis",
+    author: "Alexandros",
   },
   {
     text: "Food is absolutely delicious. There are lots of choices. I had cod chips and mushy peas and ate every bit. No complaints at all. It is rare I think for a chippy to cook good fish and chips. They are usually only good at one but the Ferryhill is good at both.",
@@ -24,6 +24,10 @@ export const reviews: Review[] = [
   },
   {
     text: "Lovely fish and chips! A clean, well organised shop and the girl serving was super friendly too. We'll be making this our regular on the back of this experience 🙂",
-    author: "Nick Kendal",
+    author: "Nick",
+  },
+  {
+    text: "Lovely fish and chips. And on Monday and Tuesday you can have Gluten-Free fish and chips, and curry sauce. They even have gluten free vinegar. Perfect!",
+    author: "Sherilee",
   },
 ];
